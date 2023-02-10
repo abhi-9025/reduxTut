dd02a9ef97144cf81714348513ab1a5e4f22ba7e
Abhishek bharti <abhisaroj192001@gmail.com>
2023-02-11 02:19:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ToastContainer } from 'react-toastify';
+
+
+const App=()=> {
+  return (
+    <div className="App">
+      <ToastContainer/>
+      <h1>Hi there</h1>
+    </div>
+  );
+}
+
+export default App;
